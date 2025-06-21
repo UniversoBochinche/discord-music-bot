9654110fb75fe11124a5f17ee157a623737feffc
Diego Silvera de Souza <diegohosilver@gmail.com>
2025-06-21 14:28:38 -0300

    Update FFmpeg options: adjust analyzeduration and remove audio codec settings for improved compatibility

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ BOT_PREFIX = '!'
 
 # FFmpeg configuration
 FFMPEG_OPTIONS = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 32 -analyzeduration 100000',
-    'options': '-vn -b:a 64k -application audio -threads 1 -ac 2 -ar 48000 -c:a libopus'
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 32 -analyzeduration 0',
+    'options': '-vn -b:a 64k -application audio -threads 1'
 }
